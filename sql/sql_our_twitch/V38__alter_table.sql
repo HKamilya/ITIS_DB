0335f03b1c5355cf19165836ba401ef03eb31789
HKamilya <33655453+HKamilya@users.noreply.github.com>
2020-11-17 19:33:12 +0300

    Update V38__alter_table.sql

--- sql/sql_our_twitch/V38__alter_table.sql
+++ sql/sql_our_twitch/V38__alter_table.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `our_twitch`.`channel_chat_message_smile` 
 ADD COLUMN `emoticon_id` INT(11) NOT NULL AFTER `channel_chat_message_id`,
-ADD INDEX `fk_channel_chat_message_emoticon_emoticon1_idx` (`emoticon_id` ASC) VISIBLE;
+ADD INDEX `fk_channel_chat_message_emoticon_emoticon1_idx` (`emoticon_id` ASC) VISIBLE
 , RENAME TO  `our_twitch`.`channel_chat_message_emoticon` ;
 
 ALTER TABLE `our_twitch`.`channel_chat_message_smile` 
